9cf831a2cf15e70437747617214a86a07e9b271b
AhmedAymanBfcai <ahmedaymanbfcai123@gmail.com>
2024-09-12 08:00:12 +0300

    Refactored Ingress resource, Error solved

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,9 +5,14 @@ const app = express();
 app.use(json());
 
 app.get('/api/users/currentuser', (req, res) => {
-    res.send("Hi There#!")
+    res.send("Hi There!")
+})
+
+app.get('/', (req, res) => {
+    console.log(req.url)
+    res.send("Hi");
 })
 
 app.listen(3000, () => {
-    console.log("Server is up on port 3000")
+    console.log("Server is up on port 3000!!")
 })
